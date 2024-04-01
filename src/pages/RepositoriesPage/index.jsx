af38cf341ddd0cc244d9c2a83cba2056eaf23410
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-01 19:30:18 -0300

    Creating 'Filters section'

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import { Container, Sidebar, Main } from './styles'
 import Profile from './Profile'
+import Filter from './Filter'
 
 export default function RepositoriesPage() {
     return (
         <Container>
             <Sidebar>
                 <Profile />
+                <Filter />
             </Sidebar>
 
             <Main>
