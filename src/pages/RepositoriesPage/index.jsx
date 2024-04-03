19bb5485e453b0289042d47c63c6f4c71dc4dc5c
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-04-03 14:57:49 -0300

    Finishing API Mock requests

--- src/pages/RepositoriesPage/index.jsx
+++ src/pages/RepositoriesPage/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 
 import { Loading, Container, Sidebar, Main } from './styles'
-import { getUser, getLangsFrom } from '../../services/api'
+import { getUser, getRepos, getLangsFrom } from '../../services/api'
 
 import Profile from './Profile'
 import Filter from './Filter'
@@ -9,80 +9,27 @@ import Repositories from './Repositories'
 
 export default function RepositoriesPage() {
     const [user, setUser] = useState()
+    const [repositories, setRepositories] = useState()
+    const [languages, setLanguages] = useState()
     const [currentLanguage, setCurrentLanguage] = useState()
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         const loadData = async () => {
-            const [userResponse] = await Promise.all([
-                getUser('PedroMarcelino7')
+            const [userResponse, repositoriesResponse] = await Promise.all([
+                getUser('PedroMarcelino7'),
+                getRepos('PedroMarcelino7')
             ])
 
             setUser(userResponse.data)
+            setRepositories(repositoriesResponse.data)
+            setLanguages(getLangsFrom(repositoriesResponse.data))
 
             setLoading(false)
         }
 
         loadData()
     }, [])
-
-    // const user = {
-    //     login: 'PedroMarcelino7',
-    //     name: 'Pedro Paulo Marcelino',
-    //     avatar_url: 'https://avatars.githubusercontent.com/u/87608270?v=4',
-    //     followers: 0,
-    //     following: 0,
-    //     company: null,
-    //     blog: 'https://pedromarcelino7.github.io/Portfolio',
-    //     location: 'Lorena',
-    // }
-
-    const repositories = [
-        {
-            id: '1',
-            name: 'Repository 1',
-            description: 'Descrição',
-            html_url: 'https://pedromarcelino7.github.io/Portfolio',
-            language: 'JavaScript',
-        },
-        {
-            id: '2',
-            name: 'Repository 2',
-            description: 'Descrição',
-            html_url: 'https://pedromarcelino7.github.io/Pokedex',
-            language: 'React',
-        },
-        {
-            id: '3',
-            name: 'Repository 3',
-            description: 'Descrição',
-            html_url: 'https://pedromarcelino7.github.io/MoviesAPI',
-            language: 'JavaScript',
-        },
-        {
-            id: '4',
-            name: 'Repository 4',
-            description: 'Descrição',
-            html_url: 'https://pedromarcelino7-ebook-website.vercel.app/',
-            language: 'HTML',
-        },
-        {
-            id: '5',
-            name: 'Repository 5',
-            description: 'Descrição',
-            html_url: 'https://pedromarcelino7.github.io/QRCodeGenerator',
-            language: 'VUE',
-        },
-        {
-            id: '6',
-            name: 'Repository 6',
-            description: 'Descrição',
-            html_url: 'https://pedromarcelino7.github.io/BootstrapMiniChallenges',
-            language: 'Bootstrap',
-        },
-    ]
-
-    const languages = getLangsFrom(repositories)
 
     const onFilterClick = (language) => {
         setCurrentLanguage(language)
